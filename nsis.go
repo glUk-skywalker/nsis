4e4f9786a4e461ded4d4c58b9c9303134276b470
Oleg Ivanov <ivanov@megaputer.ru>
2019-05-31 16:17:29 +0300

    silent installation to the passed dir

--- nsis.go
+++ nsis.go
@@ -1,5 +1,19 @@
 package nsis
 
+import (
+	"fmt"
+	"os/exec"
+)
+
 type NSIS struct {
 	installerPath string
+}
+
+func (i NSIS) InstallTo(path string) error {
+	cmd := exec.Command(i.installerPath, "/S", "/NCRC", fmt.Sprintf("/D=%s", path))
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("installer running failed: %s", err)
+	}
+	return nil
 }
